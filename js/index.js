9139ec9570cc664a66a1504d6cac51dc27949741
anniesnoopymd <anniesnoopymd@gmail.com>
2018-11-01 17:12:11 +0800

    onclick exchange the o and x

--- js/index.js
+++ js/index.js
@@ -2,8 +2,22 @@ var blocks = { type: 0 };
 var vm = new Vue({
    el: "#app",
    data: {
-      blocks: Array.from({ length: 9 }, function () {
-         return {
-            type: 1 };
+      blocks: [],
+      turn: -1 },
 
-      }) } });
+   mounted: function mounted() {
+      this.restart();
+   },
+   methods: {
+      restart: function restart() {
+         this.blocks = Array.from({ length: 9 }, function () {
+            return {
+               type: 0 };
+
+         });
+      },
+      player_go: function player_go(block) {
+         block.type = this.turn;
+         this.turn = -this.turn;
+
+      } } });
